d692c9ad8441a5f1db718c4b4f449aed59a67ce5
Mathias Mueller <mathias.mueller@uzh.ch>
2020-04-02 15:12:15 +0000

    init

---
+++ scripts/download_data.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+scripts=`dirname "$0"`
+base=$scripts/..
+
+data=$base/data
+
+mkdir -p $data
+
+tools=$base/tools
+
+# link default training data for easier access
+
+mkdir -p $data/wikitext-2
+
+for corpus in train valid test; do
+    ln -snf $tools/pytorch-examples/word_language_model/data/wikitext-2/$corpus.txt $data/wikitext-2/$corpus.txt
+done
+
+# download a different interesting data set!
+
+mkdir -p $data/trump
+
+mkdir -p $data/trump/raw
+
+wget https://raw.githubusercontent.com/ryanmcdermott/trump-speeches/master/speeches.txt
+mv speeches.txt $data/trump/raw
+
+# preprocess slightly
+
+cat $data/trump/raw/speeches.txt | python $base/scripts/preprocess_raw.py > $data/trump/raw/speeches.cleaned.txt
+
+# split into train, valid and test
+
+head -n 500 $data/trump/raw/speeches.cleaned.txt > $data/trump/valid.txt
+head -n 1000 $data/trump/raw/speeches.cleaned.txt | tail -n 500 > > $data/trump/test.txt
+tail -n 3260 $data/trump/raw/speeches.cleaned.txt > $data/trump/train.txt
